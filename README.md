6960c4a8a92eae54d728db1f40eccd9f81b8058a
Teju <tejumoluwaogunleye@gmail.com>
2025-09-02 20:57:17 +0100

    Added permanent storage to cloud (AWS)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # File Metadata Microservice
 
 This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+Technologies used for Modification:
+AWS services-S3 for storage, IAM for access Management
+Node.js - backend
+AWS SDK for Javasceipt to interact with AWS from my app.
--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var cors = require('cors');
+var app = express();
 require('dotenv').config();
 let multer = require('multer')
-let upload = multer({dest: 'uploads/'})
+let AWS = require('aws-sdk')
+let fs = require('fs')
+let mongoose = require('mongoose');
+const { error } = require('console');
 
-var app = express();
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+
+const fileSchema = new mongoose.Schema({
+  filename: String,
+  mimetype: String,
+  size: Number,
+  url: String,
+  uploadDate: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const File = mongoose.model('File', fileSchema)
+const s3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY, 
+  region: process.env.REGION
+})
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const upload = multer({storage: multer.memoryStorage()});
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -19,6 +49,33 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
     "type": req.file.mimetype,
     "size": req.file.size
   })
+})
+
+app.post('/upload', upload.single('upfile'), async (req, res) => {
+  const params = {
+    Bucket: process.env.BUCKET,
+    Key: Date.now() + '-' + req.file.originalname,
+    Body: req.file.buffer
+  };
+
+  s3.upload(params, async(err, data)=>{
+    if (err) return res.status(500).send(err);
+
+    const newFile = new File({
+      filename: req.file.originalname,
+      mimetype: req.file.mimetype,
+      size: req.file.size,
+      url: data.Location
+    });
+
+    await newFile.save();
+    res.json({message: 'File uploaded!', file: newFile});
+  })
+})
+
+app.get('/files', async(req, res) => {
+  const files = await File.find();
+  res.json(files)
 })
 
 
